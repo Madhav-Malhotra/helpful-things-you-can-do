c6a955e0620d1a885b1681d372015e04856f8d80
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-05-20 19:52:53 -0400

    Fixed modal close button

--- src/components/grid/GridModal.js
+++ src/components/grid/GridModal.js
@@ -2,10 +2,9 @@ import React from 'react'
 import ExtraInfo from '../functional/ExtraInfo';
 
 export default function GridModal(props) {  
-  
   return (
     <div className='tile-modal' id={props.idProp} key={props.idProp}>
-      <img alt='close button' className='close-modal-button' src="./cross.png" onClick={() => props.setModalID}/>
+      <img alt='close button' className='close-modal-button' src="./cross.png" onClick={() => props.setModalID(null)}/>
       <h3>{props.Description}</h3>
       <p className="detail">{props.Detailed}</p>
       <ExtraInfo {...props}/>
